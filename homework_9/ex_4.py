2c05ba9071f614820d939e663b9041d1a5836f04
Mariia Movchan <mariia.sergeevna.k@gmail.com>
2023-02-25 10:46:06 +0200

    Improve hw_9

--- homework_9/ex_4.py
+++ homework_9/ex_4.py
@@ -1,15 +1,15 @@
 def lchain(*iterables) -> list:
     result = []
     for el in iterables:
-        result.extend([*el])
+        result.extend(el)
     return result
 
 
 def test_lchain_with_multiple_iterable():
-    test_data = [1, 2, 3], {'5': 5}, tuple(), (6, 7), range(8, 10)
-    expected_result = [1, 2, 3, '5', 6, 7, 8, 9]
+    test_data = [1, 2, 3], {"5": 5}, tuple(), (6, 7), range(8, 10)
+    expected_result = [1, 2, 3, "5", 6, 7, 8, 9]
     error_msg = "Result must be '[1, 2, 3, '5', 6, 7, 8, 9]'"
-    assert lchain(test_data) == expected_result, error_msg
+    assert lchain(*test_data) == expected_result, error_msg
 
 
 if __name__ == "__main__":
--- homework_9/ex_5.py
+++ homework_9/ex_5.py
@@ -1,16 +1,19 @@
-def group_by_surname(list_of_enrollees: list):
-    first_words = [i.split()[1][0].upper() for i in list_of_enrollees]
-    groups = {
-        "ABCDEFGHI": 0,
-        "JQKLMNOP": 0,
-        "QRST": 0,
-        "UVWXYZ": 0,
-    }
-    for word in first_words:
-        for key in groups.keys():
-            if word in key:
-                groups[key] += 1
-    return groups.values()
+def group_by_surname(list_of_enrollees: list) -> list:
+    result = [0] * 4
+    group_1 = "ABCDEFGHI"
+    group_2 = "JQKLMNOP"
+    group_3 = "QRST"
+    for student in list_of_enrollees:
+        first_word = student.split()[1][0].upper()
+        if first_word in group_1:
+            result[0] += 1
+        elif first_word in group_2:
+            result[1] += 1
+        elif first_word in group_3:
+            result[2] += 1
+        else:
+            result[3] += 1
+    return result
 
 
 def main():
