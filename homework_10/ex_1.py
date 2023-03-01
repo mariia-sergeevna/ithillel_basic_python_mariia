61a05c9886ff929e70ab64d58a9400771ed3a808
Mariia Movchan <mariia.sergeevna.k@gmail.com>
2023-03-01 23:32:01 +0200

    Improve test for ex_1

--- homework_10/ex_1.py
+++ homework_10/ex_1.py
@@ -29,19 +29,13 @@ def copydeep(
         return copy_dict
 
 
-def test_deep_copy_with_dict():
+def test_deep_copy():
     test_data = [1, 2, [4, 5, 6], {"A": "B", "c": [3658]}, 2.0, {"e": 0}]
     test_data[3]["d"] = test_data
-    test_data[-1]["f"] = test_data[-1]
     copy = copydeep(test_data)
-    print(test_data)
-    print(copy)
-    print(copy[3]["c"] is not test_data[3]["c"])
-    print(copy[3]["d"] is not test_data[3]["d"])
-    print(copy[3]["d"] is copy)
-    print(copy[-1] is not test_data[-1])
-    print(copy[-1] is copy[-1]["f"])
+
+    assert (copy[3]["d"] is not test_data[3]["d"]) is True, "Result must be true"
 
 
 if __name__ == "__main__":
-    test_deep_copy_with_dict()
+    test_deep_copy()
